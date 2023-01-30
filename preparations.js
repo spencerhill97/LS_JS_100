91595e6c15dca20a20c01b9f78843e76f50aeb70
Spencer Hill <spencerahill97@gmail.com>
2023-01-30 16:01:31 -0600

    These are the preparation exercises with answers. Problems 1 through 4 involved terminal exercises and creating small JS files.

---
+++ preparations.js
@@ -0,0 +1,88 @@
+// Problem 5:
+
+/*
+Identify the Constructors for each of the following methods and classify each method as either a "Static" 
+or a "Prototype" method:
+    
+  Methods:
+  substring
+  create
+  fromCharCode
+  slice
+  toString
+
+  Answers:
+  1. string - prototype
+  2. object - static
+  3. string - static
+  4. slice - array, string
+  5. toString - string, object, array, number
+*/
+
+// Problem 6:
+
+/*
+Which of the following names satisfy the style guidelines for variable names? 
+For the purposes of this question, constants are not variables.
+
+  Names:
+  index -> legal
+  CatName
+  snake_case
+  lazyDog -> legal
+  quick_Fox
+  1stCharacter
+  operand2 -> legal
+  BIG_NUMBER
+
+*/
+
+// Problem 7:
+
+/*
+Which of the following names satisfy the style guidelines for function names?
+
+  Names:
+  index -> good
+  CatName -> good for constructor functions
+  snake_case
+  lazyDog -> good
+  quick_Fox
+  1stCharacter
+  operand2 -> good
+  BIG_NUMBER
+
+*/
+
+// Problem 8:
+
+/*
+Which of the following names satisfy the style guidelines for constants used to represent magic numbers?
+  
+  Names:
+  index
+  CatName
+  snake_case
+  lazyDog
+  quick_Fox
+  1stCharacter
+  operand2
+  BIG_NUMBER --> good
+*/
+
+// Problem 9:
+
+/*
+Which of the following names don't satisfy the style guidelines for naming variables, functions, or constants?
+
+  Names:
+  index
+  CatName
+  snake_case --> not legal
+  lazyDog
+  quick_Fox -- not legal
+  1stCharacter --> not legal
+  operand2
+  BIG_NUMBER
+
+*/
